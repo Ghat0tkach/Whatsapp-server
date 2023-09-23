1cf07a054b7ca96587e0f1679c99c96139c0137e
Vikramaditya <awesomevikram3@gmail.com>
2023-09-23 23:02:02 +0530

    feat:added cross-env , middlewares , helmet and others
    Signed-off-by: Vikramaditya <awesomevikram3@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,45 @@
 import  express  from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors"
+//create express app
 const app=express();
-app.get('/',(req,res)=>{
+
+
+//morgan
+if(process.env.NODE_ENV !== "production"){
+    app.use(morgan("dev"))
+}
+
+//Helmet
+app.use(helmet())
+
+//parse json request  body
+app.use(express.urlencoded({extended:true}))
+
+//sanitize request data
+app.use(mongoSanitize())
+
+//Enable cookie parser
+app.use(cookieParser());
+
+
+//gzip compression
+app.use(compression())
+
+
+//file upload
+app.use(fileUpload({
+    useTempFiles:true,
+}))
+
+//cors
+app.use(cors())
+app.post ('/',(req,res)=>{
     res.send("hello from server")
 })
 export default app;
