63c260fa1b360d553f0c94b7dab4235e8bf7e711
Vikramaditya <awesomevikram3@gmail.com>
2023-11-04 19:28:46 +0530

    feat: added login , register and logout function
    Signed-off-by: Vikramaditya <awesomevikram3@gmail.com>

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,7 +1,12 @@
-import { sign } from "../utils/tokenutil.js";
+import { sign, verify } from "../utils/tokenutil.js";
 
 export const generateToken=async(payload,expiresIn,secret)=>{
     let token=await sign(payload,expiresIn,secret);
     return token;
 }
 
+
+export const verifyToken=async(token,secret)=>{
+    let check =await verify(token,secret);
+    return check;
+}
